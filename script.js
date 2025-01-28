4ce58c3a9ff6a772c2c99dbaa44f407ffb6915bf
mila88111 <cg47078868@gmail.com>
2025-01-28 11:31:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script para manejar la interacción con el botón
+document.getElementById('btn-click').addEventListener('click', function() {
+    alert('¡Gracias por contactarnos! Pronto nos pondremos en contacto contigo.');
+});
